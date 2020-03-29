d67f66a0a2b620c1cf6d52fe46e661204832a26c
FFuuZZuu <47675688+FFuuZZuu@users.noreply.github.com>
2020-03-29 16:01:38 +0100

    Added Webth Ore

---
+++ Tiles/WebthOre.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+using static Terraria.ModLoader.ModContent;
+
+namespace Webth.Tiles
+{
+    public class WebthOre : ModTile
+    {
+        public override void SetDefaults()
+        {
+            TileID.Sets.Ore[Type] = true;
+            Main.tileSpelunker[Type] = true;
+            Main.tileValue[Type] = 420; // Metal detector value
+            Main.tileShine2[Type] = true;
+            Main.tileShine[Type] = 975; // Dust particle frequency, higher is less
+            Main.tileMergeDirt[Type] = true;
+            Main.tileSolid[Type] = true;
+            Main.tileBlockLight[Type] = true;
+
+            minPick = 55;
+
+            ModTranslation name = CreateMapEntryName();
+            name.SetDefault("Webth Ore");
+            AddMapEntry(new Color(100, 100, 100), name);
+
+            dustType = 84;
+            drop = ItemType<Items.Placeable.WebthOreItem>();
+            soundType = 21;
+            soundStyle = 1;
+        }
+    }
+}
