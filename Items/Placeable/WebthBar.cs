b7cf6033b00a06798cb4b8e8ee4f96a8ad710fdb
FFuuZZuu <47675688+FFuuZZuu@users.noreply.github.com>
2020-03-29 17:52:45 +0100

    Added Webth Bar

---
+++ Items/Placeable/WebthBar.cs
@@ -0,0 +1,42 @@
+﻿using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+namespace Webth.Items.Placeable
+{
+    public class WebthBar : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Webth Bar");
+        }
+
+        public override void SetDefaults()
+        {
+            item.width = 1;
+            item.height = 1;
+            item.maxStack = 99;
+            item.rare = 3;
+            item.value = 1000;
+            item.useStyle = 1;
+            item.useTime = 10;
+            item.useAnimation = 15;
+            item.autoReuse = true;
+            item.useTurn = true;
+            item.consumable = true;
+            item.createTile = TileType<Tiles.WebthBar>();
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            // Takes: 4 Webth Ore
+            recipe.AddIngredient(ItemType<WebthOreItem>(), 4);
+            // In: Any Furnace
+            recipe.AddTile(TileID.Furnaces);
+            // To Make: Webth Bar
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+    }
+}
---
+++ Tiles/WebthBar.cs
@@ -0,0 +1,24 @@
+﻿using Terraria.ModLoader;
+using Terraria;
+using Microsoft.Xna.Framework;
+using static Terraria.ModLoader.ModContent;
+
+namespace Webth.Tiles
+{
+    public class WebthBar : ModTile
+    {
+        public override void SetDefaults()
+        {
+            Main.tileSolidTop[Type] = true;
+            Main.tileSolid[Type] = true;
+            Main.tileFrameImportant[Type] = true;
+            drop = ItemType<Items.Placeable.WebthBar>();
+
+            ModTranslation name = CreateMapEntryName();
+            name.SetDefault("Metal Bar");
+            AddMapEntry(new Color(175, 175, 175), name);
+
+            minPick = 55;
+        }
+    }
+}
