4779c20ee00289d9cff89e2c1c00941de8725285
FFuuZZuu <47675688+FFuuZZuu@users.noreply.github.com>
2020-03-30 14:24:16 +0100

    Added Webbeth Shortsword

--- Items/Weapons/WebbethShortsword.cs
+++ Items/Weapons/WebbethShortsword.cs
@@ -5,26 +5,26 @@ using static Terraria.ModLoader.ModContent;
 
 namespace Webbeth.Items.Weapons
 {
-    public class WebbethBroadsword : ModItem
+    public class WebbethShortsword : ModItem
     {
         public override void SetStaticDefaults()
         {
-            DisplayName.SetDefault("Webbeth Broadsword");
-            Tooltip.SetDefault("A sword... made of webs?!");
+            DisplayName.SetDefault("Webbeth Shortsword");
+            Tooltip.SetDefault("Stabby stab stab");
         }
 
         public override void SetDefaults()
         {
-            item.value = 10000;
+            item.value = 8000;
             item.width = 2;
             item.height = 2;
-            item.useStyle = 1;
-            item.useTime = 20;
-            item.useAnimation = 20;
+            item.useStyle = 3;
+            item.useTime = 14;
+            item.useAnimation = 14;
             item.rare = 3;
 
-            item.damage = 21;
-            item.knockBack = 5;
+            item.damage = 19;
+            item.knockBack = 3;
             item.crit = 2;
             item.melee = true;
         }
@@ -33,7 +33,7 @@ namespace Webbeth.Items.Weapons
         {
             ModRecipe recipe = new ModRecipe(mod);
             // Takes: 10 Webbeth Bars
-            recipe.AddIngredient(ItemType<WebbethBar>(), 10);
+            recipe.AddIngredient(ItemType<WebbethBar>(), 8);
             // In: Furnace
             recipe.AddTile(TileID.Furnaces);
             // To make: A webbeth broadsword
