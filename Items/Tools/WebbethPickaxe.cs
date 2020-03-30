bc26197f7ee3933507f61fc454f311540448e9e8
Doomboom589 <50079382+Doomboom589@users.noreply.github.com>
2020-03-30 14:52:24 +0100

    i made you drink a pickaxe

---
+++ Items/Tools/WebbethPickaxe.cs
@@ -0,0 +1,13 @@
+﻿using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Webbeth.Items.Tools
+{
+    public class WebbethPickaxe : ModItem
+    {
+        public override void SetStaticDefault()
+        {
+
+        }
+    }
+}
